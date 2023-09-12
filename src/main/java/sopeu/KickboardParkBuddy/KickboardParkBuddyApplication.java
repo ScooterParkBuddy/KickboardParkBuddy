a43df7be09e3139c67a2553c8a3954084ca9c5b8
toki0411 <toki402@naver.com>
2023-09-12 21:21:01 +0900

    chore :프로젝트 초기 설정

---
+++ src/main/java/sopeu/KickboardParkBuddy/KickboardParkBuddyApplication.java
@@ -0,0 +1,13 @@
+package sopeu.KickboardParkBuddy;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class KickboardParkBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickboardParkBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopeu/KickboardParkBuddy/KickboardParkBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package sopeu.KickboardParkBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickboardParkBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
