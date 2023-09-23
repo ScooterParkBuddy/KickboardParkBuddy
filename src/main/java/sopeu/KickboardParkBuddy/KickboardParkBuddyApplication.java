4e5f40d6557bcd312579f6ed428a0af2627b78c6
Hyejin Shin <84217409+toki0411@users.noreply.github.com>
2023-09-23 16:28:57 +0900

    feat :로그인 화면 해제

--- src/main/java/sopeu/KickboardParkBuddy/KickboardParkBuddyApplication.java
+++ src/main/java/sopeu/KickboardParkBuddy/KickboardParkBuddyApplication.java
@@ -2,6 +2,7 @@ package sopeu.KickboardParkBuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
